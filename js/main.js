2905c7f4698e1180fa2e809b6f3b987caa15d938
Brandon Copeland <br@ndoncopeland.com>
2012-08-28 00:08:36 -0500

    esri api 3.0
    using dojo amd over require.js since esri api now includes dojo 1.7

--- js/main.js
+++ js/main.js
@@ -1,18 +1,31 @@
-require.config({
-	'paths': {
-		'Underscore': 'http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.3.3/underscore-min',
-		'Backbone': 'http://cdnjs.cloudflare.com/ajax/libs/backbone.js/0.9.2/backbone-min'
-	},
-	'shim': {
-		Backbone: {
-			'deps': [
-				'jquery',
-				'Underscore'
-			],
-			'exports': 'Backbone'
-		}
-	}
-});
+(function (window, require, define) {
+	var libsPath = window.location.pathname + 'js/lib'; // use full, rel is to dojo on cdn
 
-require(['jquery', 'Underscore', 'Backbone'], function ($, _, Backbone) {
-});
+	require({
+		async: true,
+    parseOnLoad: true,
+    aliases: [['text', 'dojo/text']],
+    packages: [
+      {
+        name: 'jquery',
+        location: libsPath,
+        main: 'jquery-1.8.0.min'
+      }, {
+        name: 'underscore',
+        location: libsPath,
+        main: 'underscore-min'
+      }, {
+        name: 'backbone',
+        location: libsPath,
+        main: 'backbone-min'
+      }
+    ]
+  });
+
+  define.amd.jQuery = true;
+
+  // can do something like...
+  // require(['jquery', 'esri', 'esri/geometry'], function ($, esri, geometry))
+  require(['jquery'], function ($) {
+  });
+}(window, require, define));
