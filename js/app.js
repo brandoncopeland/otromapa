24d5a5d585b2687215f96df0bcc89b3b397ece2d
brandoncopeland <bcopeland@gmail.com>
2012-08-28 16:11:28 -0500

    comment

--- js/app.js
+++ js/app.js
@@ -1,15 +1,16 @@
 define('app/app', ['jquery', 'esri', 'esri/geometry', 'app/mapmodel'], function ($, esri, esriGeometry, MapModel) {
 
+	var defaultExtent = new esriGeometry.Extent({
+		xmin: -10739056,
+		ymin: 3358910,
+		xmax: -10506207,
+		ymax: 3602617,
+		spatialReference: { wkid: '3857' }
+	});
+
 	var init = function () {
-		var map = new MapModel({
-			fullExtent: new esriGeometry.Extent({
-				xmin: -10739056,
-				ymin: 3358910,
-				xmax: -10506207,
-				ymax: 3602617,
-				spatialReference: { wkid: '3857' }
-			})
-		});
+		// a couple options here... can just create custom app.js for each project (current winner) or load from some config.json
+		var map = new MapModel({ fullExtent: defaultExtent });
 	};
 
 	return {
