a64b00aed05f6f07b3053e34f58b48eb822e3ab6
brandoncopeland <bcopeland@gmail.com>
2012-08-31 11:50:13 -0500

    tooltips implemented on maptoolsview

--- js/maptoolsview.js
+++ js/maptoolsview.js
@@ -1,18 +1,36 @@
-define('views/maptoolsview', ['jquery', 'underscore', 'backbone', 'text!templates/maptoolstemplate.html'], function ($, _, Backbone, mapToolsTemplate) {
+define('views/maptoolsview', ['jquery', 'underscore', 'backbone', 'text!templates/maptoolstemplate.html', 'js/plugins/jquery.tools.min.js'], function ($, _, Backbone, mapToolsTemplate) {
 	'use strict';
+
+	var toolTipSettings = {
+		effect: 'fade',
+		fadeOutSpeed: 100,
+		predelay: 600,
+		opacity: 0.9,
+		position: 'center right',
+		offset: [12, -6],
+		events: {
+			def: 'mouseenter',
+			input: 'mouseenter,click mouseleave',
+			widget: 'mouseenter,click mouseleave',
+			tooltip: ''
+		}
+	};
 
 	// TODO. these can be in their own module, so anyone can add to the array
 	var tools = [{
 		cl: 'zoomin',
 		val: 'Zoom In',
+		description: 'Go in one scale level',
 		clickHandler: 'doZoomIn'
 	}, {
 		cl: 'zoomout',
 		val: 'Zoom Out',
+		description: 'Move out one scale level',
 		clickHandler: 'doZoomOut'
 	}, {
 		cl: 'zoominitial',
 		val: 'Zoom In',
+		description: 'Go to the initial extent used when the map first loaded',
 		clickHandler: 'doZoomInitial'
 	}];
 
@@ -23,6 +41,9 @@ define('views/maptoolsview', ['jquery', 'underscore', 'backbone', 'text!template
 		template: _.template(mapToolsTemplate),
 		render: function () {
 			this.$el.html(this.template({ tools : tools }));
+
+			$(this.$el.find('[title]')).tooltip(toolTipSettings);
+
 			return this;
 		},
 		events: function () {
