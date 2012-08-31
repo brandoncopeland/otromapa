d1f60c1d597311c4e04db2cfc85a7237b98e047e
Brandon Copeland <br@ndoncopeland.com>
2012-08-31 00:35:00 -0500

    Current basemap switcher implementation complete
    will be adding more features later, but basic implementation works well
    for now

--- js/basemappickerview.js
+++ js/basemappickerview.js
@@ -1,8 +1,16 @@
+// basemappickerview.js
+// BaseMapPickerView collects all LayerModels in the MapModel's layers w/ isBasemap === true, 
+//   renders in list, and ensures exactly 1 is visible at any time.
+// Currently visible item in list tagged w/ 'selected' class.
+
 define('views/basemappickerview', ['jquery', 'underscore', 'backbone', 'text!templates/basemapitemtemplate.html'], function ($, _, Backbone, itemTemplate) {
 	'use strict';
 
+	var selectedClass = 'selected';
+
+	// view representing single basemap selector item
+	// collection - LayerModelCollection, model - LayerModel
 	var BaseMapItemView = Backbone.View.extend({
-		tagName: 'li',
 		template: _.template(itemTemplate),
 		render: function () {
 			this.$el.html(this.template({ basemap : this.model }));
@@ -25,23 +33,52 @@ define('views/basemappickerview', ['jquery', 'underscore', 'backbone', 'text!tem
     }
 	});
 
+	// view representing collection of basemap selector items
 	// collection should be LayerModelCollection, no model
+	// TODO. could probably also handle edge cases...
+	//   what if current basemap is removed? what if new visible basemap added?
 	var BaseMapPickerView = Backbone.View.extend({
 		initialize: function () {
 			var self = this;
 			self.collection.bind('all', self.render, self); // if the layer collection changes, just redraw everything
+
+			// make sure that 1 and only 1 is visible
+			// TODO. this is pretty clumbsy. refactor to something a little more elegant.
+			var bases = self.collection.baseMaps();
+			var isViz = false;
+			_.each(self.collection.baseMaps(), function (layer) {
+				var esriLayer = layer.get('esriLayer');
+				if (isViz) {
+					esriLayer.hide();
+				} else {
+					if (esriLayer.visible) {
+						isViz = true;
+					}
+				}
+			});
+			if (!isViz && bases.length > 0) {
+				_.first(bases).get('esriLayer').show();
+			}
+
 			self.render();
+		},
+		events: {
+			'click li': 'updateActive'
 		},
 		render: function () {
 			var self = this;
 
 			var bases = self.collection.baseMaps();
 
-			if (bases.length > 1) { // only draw if there are basemaps
+			if (bases.length > 1) {
 				var $list = $('<ul>');
-
 				_.each(bases, function (layer) {
-					var view = new BaseMapItemView({ model: layer, collection: self.collection });
+					var view = new BaseMapItemView({
+						model: layer,
+						collection: self.collection,
+						tagName: 'li',
+						className: layer.get('esriLayer').visible ? selectedClass : ''
+					});
 					$list.append(view.render().el);
 				});
 
@@ -49,11 +86,19 @@ define('views/basemappickerview', ['jquery', 'underscore', 'backbone', 'text!tem
 
 				self.$el.removeClass('hidden');
 			} else {
+				// no need for this view if there are less than 2 basemaps...
 				self.$el.html('');
 				self.$el.addClass('hidden');
 			}
 
 			return self;
+		},
+		updateActive: function (evt) {
+			// also handle click here... (in addition to item views)
+			// Item view knows how to interact with the model (draw basemap) for each input click
+			// Picker view knows how to manage all child views (dom elements), that they're 'li's, and their classes
+			this.$el.find('li').removeClass(selectedClass);
+			$(evt.currentTarget).addClass(selectedClass);
 		}
 	});
 
