8f4c44693b57a25fb4978788c4233608e0b25c7e
brandoncopeland <bcopeland@gmail.com>
2012-09-26 17:02:31 -0500

    comment

--- js/app.js
+++ js/app.js
@@ -11,7 +11,6 @@ define('app/app', ['jquery', 'esri', 'esri/geometry', 'models/mapmodel', 'models
 
 	var init = function () {
 
-		// a couple options for app specific config... can just create custom app.js for each project (current winner) or load from some config.json
 		var map = new MapModel({
 			fullExtent: defaultExtent
 		});
