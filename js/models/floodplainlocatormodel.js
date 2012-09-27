ddef3bb5849898ea6508938d2846fd33c230ac55
Brandon Copeland <br@ndoncopeland.com>
2012-09-27 11:58:33 -0500

    flood zone search and message

--- js/models/floodplainlocatormodel.js
+++ js/models/floodplainlocatormodel.js
@@ -2,12 +2,17 @@
 // listens for new features added to MapFeatureModelCollection, queries floodplain feature service, and appends floodplain attributes
 // features - MapFeatureModelCollection
 // floodplainServiceUrl - url to ESRI feature layer
-define('models/floodplainlocatormodel', ['jquery', 'underscore', 'backbone', 'esri'], function ($, _, Backbone, esri) {
+define('models/floodplainlocatormodel', ['jquery', 'underscore', 'backbone', 'esri', 'text!data/floodzones.json', 'esri/tasks/query'], function ($, _, Backbone, esri, zoneData) {
 	'use strict';
 
 	var FloodplainLocatorModel = Backbone.Model.extend({
+		defaults: {
+			floodMessageAttributeField: 'floodMessage',
+			floodZoneField: 'FLD_ZONE'
+		},
 		initialize: function () {
 			var self = this;
+			self._zoneData = $.parseJSON(zoneData);
 			var features = self.get('features');
 			if (features) {
 				features.on('add', function (item) {
@@ -21,7 +26,28 @@ define('models/floodplainlocatormodel', ['jquery', 'underscore', 'backbone', 'es
 			}
 		},
 		locateFloodplainForFeature: function (feature) {
-			console.log('floodplain located for feature ' + feature.get('props').name);
+			var self = this;
+			var serviceUrl = self.get('floodplainServiceUrl');
+			if (serviceUrl) {
+				var task = new esri.tasks.QueryTask(serviceUrl);
+				var query = new esri.tasks.Query();
+				query.geometry = feature.get('geometry');
+				query.returnGeometry = false;
+				query.outFields = ['*'];
+				task.execute(query, function (result) {
+					var zoneMessage;
+					if (result.features.length === 0) {
+						zoneMessage = 'Flood Zone could not be determined for this location.';
+					} else if (result.features.length === 1) {
+						var featureZone = result.features[0].attributes[self.get('floodZoneField')];
+						zoneMessage = 'Located in Flood Zone ' + featureZone;
+						if (self._zoneData[featureZone]) {
+							zoneMessage = zoneMessage + '. ' + self._zoneData[featureZone].sfhaDescription;
+						}
+					}
+					feature.get('props')[self.get('floodMessageAttributeField')] = zoneMessage;
+				});
+			}
 		}
 	});
 
