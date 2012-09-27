77bd0fe334c3c083e75d9241f69c9473f5c3977f
Brandon Copeland <br@ndoncopeland.com>
2012-09-27 09:58:23 -0500

    floodplainlocatormodel added

---
+++ js/models/floodplainlocatormodel.js
@@ -0,0 +1,9 @@
+// floodplainlocatormodel
+// listens for new features added to MapFeatureModelCollection, queries floodplain feature service, and appends floodplain attributes
+// features - MapFeatureModelCollection
+// floodplainServiceUrl - url to ESRI feature layer
+define('models/floodplainlocatormodel', ['jquery', 'underscore', 'backbone', 'esri'], function ($, _, Backbone, esri) {
+	'use strict';
+
+
+});
